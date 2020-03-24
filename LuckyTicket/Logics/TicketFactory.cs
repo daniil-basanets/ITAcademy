2c5d64930e60e998992bde7c50e0afa107b231f0
daniil-basanets <daniil.basanets@gmail.com>
2020-03-24 22:32:10 +0200

    Finish lucky ticket

---
+++ LuckyTicket/Logics/TicketFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LuckyTicket.Models;
+using LuckyTicket.Implementation;
+
+namespace LuckyTicket.Logics
+{
+    static class TicketFactory
+    {
+        public static Ticket Build(LuckyType luckyType, int ticketNumber = 0)
+        {
+            switch (luckyType)
+            {
+                case LuckyType.Petersberg:
+                    return new PitersbergTicket(ticketNumber);
+                case LuckyType.Moscow:
+                    return new MoscowTicket(ticketNumber);
+                default:
+                    throw new ArgumentException("Unknown LuckyType!");
+            }
+        }
+    }
+}
--- LuckyTicket/Program.cs
+++ LuckyTicket/Program.cs
@@ -1,25 +1,64 @@
-﻿using LuckyTicket.Implementation;
+﻿using log4net;
 using LuckyTicket.Logics;
 using LuckyTicket.Models;
 using System;
+using HelpersLibrary;
+using System.IO;
+using log4net.Config;
 
 namespace LuckyTicket
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private const string NotFound = "File not found!";
+
         static void Main(string[] args)
         {
 
-            FileParser file = new FileParser("t.txt");
-            file.GetLuckyType();
-            Ticket ticket = new PitersbergTicket(000000);
-            var b = ticket.IsLucky();
-            var c = ticket.GetLuckyCount();
+            #region Initialize logger
 
-            Ticket ticket1 = new MoscowTicket(000000);
-            var b1 = ticket1.IsLucky();
-            var c1 = ticket1.GetLuckyCount();
-            Console.WriteLine("Hello World!");
+            var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new System.IO.FileInfo("Log4Net.config"));
+            log.Info("Application [FileParser] Start");
+
+            #endregion
+
+
+            #region Check input data
+
+            var fileName = Parser.TryGetString(args, 0, out ErrorCode errorCode);
+            if (errorCode != ErrorCode.Void)
+            {
+                Console.WriteLine(errorCode.GetMessage());
+
+                return;
+            }
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine(NotFound);
+
+                return;
+            }
+
+            #endregion
+
+            try
+            {
+                var ticketType = new FileParser(fileName).GetLuckyType();
+                Ticket ticket = TicketFactory.Build(ticketType);
+
+                var count = ticket.GetLuckyCount();
+
+                Console.WriteLine("Lucky tickets count:" + count);
+            }
+            catch (Exception e)
+            {
+                log.Error(e);
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
