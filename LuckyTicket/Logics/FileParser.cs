d7f9238a239d25079de71b5f73b8e8d29741ca46
daniil-basanets <daniil.basanets@gmail.com>
2020-03-24 19:49:51 +0200

    Lucky ticket add

---
+++ LuckyTicket/Logics/FileParser.cs
@@ -0,0 +1,28 @@
+﻿using LuckyTicket.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LuckyTicket.Logics
+{
+    class FileParser
+    {
+        #region Private Members
+
+        private readonly string fileName;
+
+        #endregion
+
+        public FileParser(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public LuckyType GetLuckyType()
+        {
+            StreamReader streamReader = new StreamReader(fileName);
+
+        }
+    }
+}
---
+++ LuckyTicket/Models/LuckyType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyTicket.Models
+{
+	enum LuckyType
+	{
+		Petersberg = 1,
+		Moscow = 2
+	}
+}
